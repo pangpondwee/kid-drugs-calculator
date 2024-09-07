2d29ed48f09b868005fb97bc48940de4dc4eefe1
Nat <pangpondwee@gmail.com>
2024-09-07 12:49:18 +0700

    init drug data

---
+++ data/drug.ts
@@ -0,0 +1,12 @@
+export const drugs = [
+  {
+    type: "calculateByAge",
+    displayName: "Albendazole 200mg/5ml syr",
+    originalDose: "> 2 y: 10 mL with meal single dose or BID 3 Days",
+  },
+  {
+    type: "calculateByWeight",
+    displayName: "Amoxicillin 125mg/5ml syr",
+    originalDose: "6.6 - 13.3 mg/kg/dose",
+  },
+];
