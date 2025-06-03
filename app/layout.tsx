176f3873cfd35df4e0f37f474a758d84ccd1d194
Nat <pangpondwee@gmail.com>
2025-06-03 15:37:17 +0700

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Sarabun } from "next/font/google";
 import { Providers } from "@/components/providers";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "แอปพลิเคชันคำนวณขนาดยาน้ำในเด็ก",
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="th" suppressHydrationWarning>
       <body className={`${sarabun.variable} font-sans antialiased `}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
