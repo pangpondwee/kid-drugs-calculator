839b9fbe0ef2e881df0b10652e432302f9af42c1
Nat <pangpondwee@gmail.com>
2025-10-09 22:39:03 +0700

    chore: change metadata in footer

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Page() {
       <footer className="px-4 py-1 text-xs flex flex-col items-center justify-center text-slate-600">
         <p>Version 1.1.0 (07/06/2025)</p>
         <p>Developed by Chanatkarn Leesakulpisut,</p>
-        <p>Chinnachot Saehor, Thitikarn Wichisalo</p>
       </footer>
     </div>
   );
